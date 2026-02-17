99f42e12546b1ec8e63ac926637b75c81ab5add7
nadinestrella <nadinestrella@gmail.com>
2026-02-17 12:38:51 +0100

    Initial serverless function

--- generate.ts
+++ generate.ts
@@ -8,6 +8,15 @@ if (!apiKey) {
 }
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+  //Allow CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
